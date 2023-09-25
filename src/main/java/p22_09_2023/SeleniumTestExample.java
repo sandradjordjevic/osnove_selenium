002d77762142bdb6a03d6808f1e63d5ad8779ca2
Sandra Djordjevic <sandradj2331@gmail.com>
2023-09-25 13:04:45 +0200

    Checking settings

---
+++ src/main/java/p22_09_2023/SeleniumTestExample.java
@@ -0,0 +1,22 @@
+package p22_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTestExample {
+    public static void main(String[] args) {
+        // Postavljanje ChromeDriver koristeći WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        // Kreiranje instance ChromeDriver-a
+        WebDriver driver = new ChromeDriver();
+        // Otvaranje veb stranice
+        driver.get("https://google.com");
+        // Mesto za test kod ...
+
+
+        // Zatvoranje pretrazivaca nakon sto se zavrsi testiranje
+        driver.quit();
+    }
+}
+
