68969d5f85c5e9e764e4f3d1dfc3e20871fbe421
Sandra Djordjevic <sandradj2331@gmail.com>
2023-10-01 14:30:40 +0200

    Explicit wait, waiting for some number of elements that are visible.

---
+++ src/main/java/d29_09_2023/Zadatak6.java
@@ -0,0 +1,46 @@
+package d29_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Zadatak6 {
+    public static void main(String[] args) throws InterruptedException {
+//        6.Zadatak
+//        Napisati program koji:
+//        Ucitava stranicu https://mdbootstrap.com/docs/standard/components/toasts/#section-basic-example
+//        Vrsi klik na Primary dugme, Secondary, Sucess, Danger
+//        Ceka da broj toasts-a bude 4
+//        Ispisuje poruku, toasts su prikazani
+//        Ceka da broj toasts-a bude 0
+//        Ispisuje poruku, toasts su se izgubili
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://mdbootstrap.com/docs/standard/components/toasts/#section-basic-example");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("basic-primary-trigger")).click();
+        driver.findElement(By.id("basic-secondary-trigger")).click();
+        driver.findElement(By.id("basic-success-trigger")).click();
+        driver.findElement(By.id("basic-danger-trigger")).click();
+        Thread.sleep(1000);
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("div.toast-fixed.show"), 4));
+        System.out.println("Toasts are visible.");
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("div.toast-fixed.show"), 0));
+        System.out.println("Toast are invisible.");
+        driver.quit();
+    }
+}
